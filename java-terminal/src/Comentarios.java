cc1abd8e7ed4f9536e64ad3bbd2692b2712c45b6
brunasiaovn <139784873+brunasiaovn@users.noreply.github.com>
2024-02-03 11:23:03 -0300

    commit usando o terminal pra doc

---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,16 @@
+public class Comentarios {
+    
+    public static void main(String[] args) {
+        
+    }
+
+public int somaMultiplica (int n, int x, String m){
+    int r = 0;
+    if (m == "M") {
+        r= n * x;
+    } else {
+        r = n + x;
+    }
+    return r;
+}
+}
