c2614611c2d5edbc69e4c91d2d4408eaee5f9349
brunasiaovn <139784873+brunasiaovn@users.noreply.github.com>
2024-02-07 23:08:00 -0300

    classe person

---
+++ classe-person/ClassePessoa.java
@@ -0,0 +1,13 @@
+public class ClassePessoa {
+        public static void main(String[] args) {
+        Person person = new Person();
+        }
+    
+        public static class Person {
+            String nome;
+            int idade;
+            int peso;
+            int dinheiro;
+        }
+    
+}
