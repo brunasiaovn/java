a1b86fb0ab4a1a7ce2521d3b50e3067b15744a4b
brunasiaovn <139784873+brunasiaovn@users.noreply.github.com>
2024-02-08 18:04:41 -0300

    classe e objeto

---
+++ classe-objeto/ClasseObjeto.java
@@ -0,0 +1,27 @@
+public class ClasseObjeto {
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Fish fish = new Fish();
+        Woman woman = new Woman();
+        cat.owner = woman;
+        dog.owner = woman;
+        fish.owner = woman;
+    }
+
+    public static class Cat {
+        public Woman owner;
+    }
+
+    public static class Dog {
+        public Woman owner;
+    }
+
+    public static class Fish {
+        public Woman owner;
+    }
+
+    public static class Woman {
+    }
+}
