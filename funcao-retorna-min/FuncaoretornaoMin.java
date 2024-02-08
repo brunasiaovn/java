8b8a8659ae474fb4e07dd348fd45387505f68d83
brunasiaovn <139784873+brunasiaovn@users.noreply.github.com>
2024-02-08 20:53:43 -0300

    uma função que retorne o mínimo de dois números

---
+++ funcao-retorna-min/FuncaoretornaoMin.java
@@ -0,0 +1,15 @@
+public class FuncaoretornaoMin {
+    public static int min(int a, int b) {
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(min(12, 33));
+        System.out.println(min(-20, 0));
+        System.out.println(min(-10, -20));
+    }
+}
