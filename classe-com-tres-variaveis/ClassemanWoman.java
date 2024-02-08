b60e19ede47f0466e95aabf62d91cdebe5aa7cd8
brunasiaovn <139784873+brunasiaovn@users.noreply.github.com>
2024-02-08 00:22:31 -0300

    classe com três variáveis

---
+++ classe-com-tres-variaveis/ClassemanWoman.java
@@ -0,0 +1,21 @@
+public class ClassemanWoman {
+
+    public static void main(String[] args) {
+        Woman woman = new Woman();
+        Man man = new Man();
+        woman.husband = man;
+        man.wife = woman;
+    }
+
+    public static class Man {
+        public int age;
+        public int height;
+        public Woman wife;
+    }
+
+    public static class Woman {
+        public int age;
+        public int height;
+        public Man husband;
+    }
+}
