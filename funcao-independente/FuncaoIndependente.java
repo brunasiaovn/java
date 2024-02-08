ff0823a96648d5ab64c2ff796d5f0a05632b9bb0
brunasiaovn <139784873+brunasiaovn@users.noreply.github.com>
2024-02-08 19:59:46 -0300

    Função independente

--- funcao-independente/FuncaoIndependente.java
+++ funcao-independente/FuncaoIndependente.java
@@ -1,21 +1,20 @@
-public class FuncaoIndependente {
-
+public class FuncaoIndependente
+{
     public static void main(String[] args)
-   {
+    {
         int a = 5, b = 7;
         int m = min(a, b);
-        System.out.println("The minimun is " + m);
-   }
+        System.out.println("O minimo é " + m);
+    }
 
-   public static int min(int c, int d)
-   {
-       int m2;
-       if (c < d)
-           m2 = c;
-       else
-           m2 = d;
-    
-       return m2;
+    public static int min(int c, int d)
+    {
+        int m2;
+        if (c < d)
+            m2 = c;
+        else
+            m2 = d;
+
+        return m2;
     }
 }
-
