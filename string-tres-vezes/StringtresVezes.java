80bf285ab12cd84fffbfc208bcd99f475b96e8e5
brunasiaovn <139784873+brunasiaovn@users.noreply.github.com>
2024-02-10 00:47:28 -0300

    exibir a string passada 3 vezes

---
+++ string-tres-vezes/StringtresVezes.java
@@ -0,0 +1,11 @@
+public class StringtresVezes {
+    public static void print3(String s) {
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        print3("Repetition is the mother of all learning");
+    }
+}
