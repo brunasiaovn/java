062417a5c5f6c4cccfd4bc51e42d09444fc2ca9e
brunasiaovn <139784873+brunasiaovn@users.noreply.github.com>
2024-02-10 01:15:27 -0300

    exibir a string três vezes , mas na mesma linha.

---
+++ string-tres-vezes/StringtresvezesmesmaLinha.java
@@ -0,0 +1,14 @@
+public class StringtresvezesmesmaLinha {
+    public class Solution {
+        public static void print3(String s) {
+            System.out.print(s + " ");
+            System.out.print(s + " ");
+            System.out.println(s);
+        }
+
+        public static void main(String[] args) {
+            print3("window");
+            print3("file");
+        }
+    }
+}
