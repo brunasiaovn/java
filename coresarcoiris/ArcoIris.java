55171dd666f40c0241626977e09e2932574c2b7c
brunasiaovn <139784873+brunasiaovn@users.noreply.github.com>
2024-02-14 20:31:13 -0300

    7 objetos para exibir as 7 cores do arco-íris

---
+++ coresarcoiris/ArcoIris.java
@@ -0,0 +1,54 @@
+package coresarcoiris;
+
+public class ArcoIris {
+    public static void main(String[] args) {
+        new Vermelho();
+        new Laranja();
+        new Amarelo();
+        new Verde();
+        new Azul();
+        new Indigo();
+        new Violeta();
+    }
+
+    public static class Vermelho {
+        public Vermelho() {
+            System.out.println("Vermelho");
+        }
+    }
+
+    public static class Laranja {
+        public Laranja() {
+            System.out.println("Laranja");
+        }
+    }
+
+    public static class Amarelo {
+        public Amarelo() {
+            System.out.println("Amarelo");
+        }
+    }
+
+    public static class Verde {
+        public Verde() {
+            System.out.println("Verde");
+        }
+    }
+
+    public static class Azul {
+        public Azul() {
+            System.out.println("Verde");
+        }
+    }
+
+    public static class Indigo {
+        public Indigo() {
+            System.out.println("Indigo");
+        }
+    }
+    public static class Violeta {
+        public Violeta(){
+            System.out.println("Violeta");
+        }
+    }
+}
---
+++ coresarcoiris/readme.md
@@ -0,0 +1,20 @@
+## Crie 7 objetos para exibir as 7 cores do arco-íris na tela.
+Exemplo de saída:
+Vermelho
+Laranja 
+Amarelo
+Verde
+Azul
+Índigo 
+Violeta 
+
+# Requisitos:
+•   O programa deve gerar texto.
+
+•   Você precisa criar 7 objetos diferentes responsáveis ​​pelas cores.
+
+•   A ordem em que os objetos são criados deve corresponder à ordem das cores do arco-íris.
+
+•   Não altere as classes responsáveis ​​pelas cores.
+
+•   O texto exibido deve corresponder às condições da tarefa.
