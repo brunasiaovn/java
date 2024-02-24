580067fd1ab18a1475091d4af2bdcd0ffbd1c1e9
Bruna <bruna.dev@hotmail.com>
2024-02-23 21:04:21 -0300

    Se existir um par de número exiba-os separadamente

---
+++ temosumpar/README.md
@@ -0,0 +1,28 @@
+##
+## Use o teclado para inserir três números inteiros . Determine se existe pelo menos um par de números iguais entre eles .
+## Se tal par existir, exiba os números separados por um espaço .
+## Se todos os três números forem iguais, exiba todos os três .
+##
+### Aqui estão alguns exemplos:
+### a) se você inserir os números
+### 1
+### 2
+### 2
+### , exibiremos:
+### 2 2
+
+
+### b) se você inserir os números
+### 2
+### 2
+### 2
+### , exibiremos:
+### 2 2 2
+##
+### Requisitos:
+### •	O programa deve ler os números do teclado.
+### •	O programa deve conter System.out.println() ou System.out.print().
+### •	Se dois números forem iguais, você deverá exibi-los na tela.
+### •	Se todos os três números forem iguais, exiba todos os três.
+### •	Se não houver números iguais, não exiba nada.
+##
---
+++ temosumpar/UmpardeNumeros.java
@@ -0,0 +1,28 @@
+package temosumpar;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UmpardeNumeros {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+
+        if (a == b && a == c) {
+            System.out.println(a + " " + b + " " + c);
+        } else {
+            if (a == b) {
+                System.out.println(a + " " + b);
+            }
+            if (a == c) {
+                System.out.println(a + " " + c);
+            }
+            if (b == c) {
+                System.out.println(b + " " + c);
+            }
+        }
+    }
+}
