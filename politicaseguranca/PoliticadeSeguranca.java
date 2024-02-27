612007f5052e8c84596d0794419fe2c963300ba7
Bruna <bruna.dev@hotmail.com>
2024-02-27 16:29:34 -0300

    Se a idade for superior a 20 anos , exiba uma msg

---
+++ politicaseguranca/PoliticadeSeguranca.java
@@ -0,0 +1,17 @@
+package politicaseguranca;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class PoliticadeSeguranca {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+        String nome = bis.readLine();
+        int idade = Integer.parseInt(bis.readLine());
+
+        if (idade > 20) {
+            System.out.println("18 é idade suficiente");
+        }
+    }
+}
---
+++ politicaseguranca/README.md
@@ -0,0 +1,7 @@
+### Use o teclado para inserir o nome e a idade . Se a idade for superior a 20 anos , exiba “ 18 é idade suficiente ”.
+##
+### Requisitos:
+### •	O programa deve ler as linhas do teclado.
+### •	O programa deve usar System.out.println() ou System.out.print().
+### •	Se a idade for superior a 20 anos, exiba "18 anos é idade suficiente" e nada mais.
+### •	Se a idade for menor ou igual a 20 anos, não exiba nada.
