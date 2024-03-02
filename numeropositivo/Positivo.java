0174d58d8af01eb25eb654c9b054d7f4bdabc29b
Bruna <bruna.dev@hotmail.com>
2024-03-02 12:00:35 -0300

    Exiba o número de números positivos no conjunto

--- numeropositivo/Positivo.java
+++ numeropositivo/Positivo.java
@@ -1,8 +1,25 @@
 package numeropositivo;
 
-public class Positivo {
-    public static void main(String[] args) throws Exception {
-        //write your code here
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Positivo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(bis.readLine());
+        int b = Integer.parseInt(bis.readLine());
+        int c = Integer.parseInt(bis.readLine());
+        int cont = 0;
+        if (a > 0) {
+            cont++;
+        }
+        if (b > 0) {
+            cont++;
+        }
+        if (c > 0) {
+            cont++;
+        }
+        System.out.println(cont);
     }
 }
