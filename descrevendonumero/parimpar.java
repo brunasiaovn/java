dd7325b0465083d833fc7d044a3b7e7871656aec
Bruna <bruna.dev@hotmail.com>
2024-03-02 11:10:10 -0300

    Insira um número no teclado de 1 a 999

--- descrevendonumero/parimpar.java
+++ descrevendonumero/parimpar.java
@@ -1,8 +1,25 @@
 package descrevendonumero;
 
-public class parimpar {
-    public static void main(String[] args) throws Exception {
-        //write your code here
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class parimpar {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(bis.readLine());
+        if (a > 0 && a < 10 && a % 2 == 0) {
+            System.out.println("even single-digit number");
+        } else if (a > 0 && a < 10 && a % 2 != 0) {
+            System.out.println("odd single-digit number");
+        } else if (a > 9 && a < 100 && a % 2 == 0) {
+            System.out.println("even two-digit number");
+        } else if (a > 9 && a < 100 && a % 2 != 0) {
+            System.out.println("odd two-digit number");
+        } else if (a > 99 && a < 1000 && a % 2 == 0) {
+            System.out.println("even three-digit number");
+        } else if (a > 99 && a < 1000 && a % 2 != 0) {
+            System.out.println("odd three-digit number");
+        }
     }
 }
