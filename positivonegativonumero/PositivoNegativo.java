3c639f2b7740b9fa3d0abec5dba2adf000700698
Bruna <bruna.dev@hotmail.com>
2024-03-03 22:23:30 -0300

    Exiba os número positivos e os número negativos

--- positivonegativonumero/PositivoNegativo.java
+++ positivonegativonumero/PositivoNegativo.java
@@ -1,8 +1,33 @@
 package positivonegativonumero;
 
-public class PositivoNegativo {
-    public static void main(String[] args) throws Exception {
-        // write your code here
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class PositivoNegativo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(bis.readLine());
+        int b = Integer.parseInt(bis.readLine());
+        int c = Integer.parseInt(bis.readLine());
+        int contPositive = 0;
+        int contNegative = 0;
+        if (a > 0) {
+            contPositive++;
+        } else if (a < 0) {
+            contNegative++;
+        }
+        if (b > 0) {
+            contPositive++;
+        } else if (b < 0) {
+            contNegative++;
+        }
+        if (c > 0) {
+            contPositive++;
+        } else if (c < 0) {
+            contNegative++;
+        }
+        System.out.println("Number of negative numbers: " + contNegative);
+        System.out.println("Number of positive numbers: " + contPositive);
     }
 }
