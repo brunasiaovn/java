b9bee4525113b807074a03f9f22042fba8a1aa00
Bruna <bruna.dev@hotmail.com>
2024-03-05 19:11:39 -0300

    Exibindo os números de 1 ao 10 usando while

--- deumaodez/UmaoDez.java
+++ deumaodez/UmaoDez.java
@@ -2,7 +2,10 @@ package deumaodez;
 
 public class UmaoDez {
     public static void main(String[] args) throws Exception {
-        //write your code here
-
+        int i = 1;
+        while (i < 11) {
+            System.out.println(i);
+            i++;
+        }
     }
 }
