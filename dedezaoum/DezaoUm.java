79cad8372a42145c7e1b4d1f52cdb5aa9b4029a3
Bruna <bruna.dev@hotmail.com>
2024-03-05 19:15:59 -0300

    exibindo os numeros de 10 ao 1  usando um while

--- dedezaoum/DezaoUm.java
+++ dedezaoum/DezaoUm.java
@@ -2,7 +2,10 @@ package dedezaoum;
 
 public class DezaoUm {
     public static void main(String[] args) throws Exception {
-        //write your code here
-
+        int i = 10;
+        while (i > 0) {
+            System.out.println(i);
+            i--;
+        }
     }
 }
