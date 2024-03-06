403cb51872b4a6cf640cf84880eeaacd297db7da
Bruna <bruna.dev@hotmail.com>
2024-03-06 15:11:00 -0300

    Usando o while para exibir a string N vezes

--- stringenumero/InserirstringeNumero.java
+++ stringenumero/InserirstringeNumero.java
@@ -1,7 +1,17 @@
 package stringenumero;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class InserirstringeNumero {
     public static void main(String[] args) throws Exception {
-        //write your code here
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+        String name = bis.readLine();
+        int i = Integer.parseInt(bis.readLine());
 
+        while (i > 0) {
+            System.out.println(name);
+            i--;
+        }
     }
 }
