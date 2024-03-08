9cc04c501f5e2404e0800fa7f4429b894b69ea5a
Bruna <bruna.dev@hotmail.com>
2024-03-07 21:59:49 -0300

    usando while para fazer um loop 10x10 de cifrões

--- cifroes/Quadrado.java
+++ cifroes/Quadrado.java
@@ -1,8 +1,18 @@
 package cifroes;
  
  public class Quadrado {
-    public static void main(String[] args) throws Exception {
-        //write your code here
+    public static void main(String[] args) {
 
+        int i = 0;
+
+        while (i < 10) {
+            int j = 0;
+            while (j < 10) {
+                System.out.print("$");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
     }
 }
