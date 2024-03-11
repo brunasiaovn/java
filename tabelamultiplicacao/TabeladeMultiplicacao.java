670456572c593e7a0cc6e70f1142864b94a38050
Bruna <bruna.dev@hotmail.com>
2024-03-10 22:43:35 -0300

    usando o while para ixibir a tabuada 10x10

--- tabelamultiplicacao/TabeladeMultiplicacao.java
+++ tabelamultiplicacao/TabeladeMultiplicacao.java
@@ -4,8 +4,18 @@ package tabelamultiplicacao;
  * TabeladeMultiplicacao
  */
 public class TabeladeMultiplicacao {
-    public static void main(String[] args) throws Exception {
-        //write your code here
+    public static void main(String[] args) throws Exception  {
+        int i = 1;
 
+        while (i < 11) {
+            int j = 1;
+            while (j < 11) {
+                System.out.print(i * j);
+                System.out.print(" ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
     }
 }
