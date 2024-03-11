8be8e9a053bc1be1d16bc90f4145a796c859cea9
Bruna <bruna.dev@hotmail.com>
2024-03-10 22:40:28 -0300

    Use um while para exibir a tabuada do 10

---
+++ tabelamultiplicacao/TabeladeMultiplicacao.java
@@ -0,0 +1,11 @@
+package tabelamultiplicacao;
+
+/**
+ * TabeladeMultiplicacao
+ */
+public class TabeladeMultiplicacao {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+
+    }
+}
