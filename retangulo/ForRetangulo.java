9849bb194ff531e00a349461154fdb60305b72f7
Bruna <bruna.dev@hotmail.com>
2024-03-14 20:15:01 -0300

    "feat: Adicionar esqueleto básico do código"

---
+++ retangulo/ForRetangulo.java
@@ -0,0 +1,11 @@
+package retangulo;
+/**
+ * ForRetangulo
+ */
+
+public class ForRetangulo {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+
+    }
+}
