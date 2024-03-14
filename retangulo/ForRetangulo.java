655cf114251375babd1b740ef970373462bfc7be
Bruna <bruna.dev@hotmail.com>
2024-03-14 20:27:13 -0300

    feat: Adicionar implementação do código para exibir um retângulo

--- retangulo/ForRetangulo.java
+++ retangulo/ForRetangulo.java
@@ -2,10 +2,21 @@ package retangulo;
 /**
  * ForRetangulo
  */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class ForRetangulo {
     public static void main(String[] args) throws Exception {
-        //write your code here
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(bis.readLine());
+        int n = Integer.parseInt(bis.readLine());
 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print('8');
+            }
+
+            System.out.println();
+        }
     }
 }
