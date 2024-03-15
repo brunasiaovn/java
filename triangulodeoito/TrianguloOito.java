7338dae09b2dd171376a0771f2da17be293e96a2
Bruna <bruna.dev@hotmail.com>
2024-03-15 19:42:15 -0300

    feat: Adicionando lógica para gerar triângulo de oitos

--- triangulodeoito/TrianguloOito.java
+++ triangulodeoito/TrianguloOito.java
@@ -2,7 +2,14 @@ package triangulodeoito;
 
 public class TrianguloOito {
     public static void main(String[] args) throws Exception {
-        //write your code here
+        int m = 10;
 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print('8');
+            }
+
+            System.out.println();
+        }
     }
 }
