a2129a64a101269a736312d636750c892e35eced
Bruna <bruna.dev@hotmail.com>
2024-03-16 22:20:59 -0300

    feat: Adicionando lógica para exibir o número do meio dos três números

--- media/Media.java
+++ media/Media.java
@@ -1,9 +1,24 @@
 package media;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * Media
  */
 public class Media {
     public static void main(String[] args) throws Exception {
-        //write your code here
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(bis.readLine());
+        int b = Integer.parseInt(bis.readLine());
+        int c = Integer.parseInt(bis.readLine());
+
+        if ((a >= b && a <= c) || (a <= b && a >= c)) {
+            System.out.println(a);
+        } else if ((b >= a && b <= c) || (b <= a && b >= c)) {
+            System.out.println(b);
+        } else if ((c >= a && c <= b) || (c <= a && c >= b)) {
+            System.out.println(c);
+        }
     }
 }
