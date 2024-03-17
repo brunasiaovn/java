4a7f2ec50b4bba6f9c09d7ba3d88ba1b95035a2b
Bruna <bruna.dev@hotmail.com>
2024-03-17 20:15:31 -0300

    feat: Adicionando lógica que calcula a soma de números lidos da entrada padrão até o usuário inserir -1

--- adicionando/InserirNumeros.java
+++ adicionando/InserirNumeros.java
@@ -1,9 +1,24 @@
 package adicionando;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * InserirNumeros
  */
 public class InserirNumeros {
-    public static void main(String[] args) throws Exception {
-    //write your code here
+        public static void main(String[] args) throws Exception {
+            BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+    
+            int sum = 0;
+            while (true) {
+                int a = Integer.parseInt(bis.readLine());
+                sum += a;
+    
+                if (a == -1) {
+                    System.out.println(sum);
+                    return;
+                }
+            }
+        }
     }
-}
