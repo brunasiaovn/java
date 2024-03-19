0cef42b49de8d046636d2aa82c3b736fc042fc7f
Bruna <bruna.dev@hotmail.com>
2024-03-19 18:04:13 -0300

    feat: Adicionando nome, idade, altura, peso e força

--- criandoumgato/Gato.java
+++ criandoumgato/Gato.java
@@ -1,6 +1,11 @@
 package criandoumgato;
 
 public class Gato {
+    public String name;
+    protected int age;
+    int weight;
+    @SuppressWarnings("unused")
+    private int strength;
     public static void main(String[] args) {
 
     }
