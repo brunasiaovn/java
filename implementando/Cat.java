31588ca131a37c3ba0cf4f6303630bdce8bd9073
Bruna <bruna.dev@hotmail.com>
2024-03-20 18:56:50 -0300

    feat: Adicionando classe cat com idade, altura, e metodo boolean

---
+++ implementando/Cat.java
@@ -0,0 +1,18 @@
+package implementando;
+
+public class Cat {
+    public int age;
+    public int weight;
+    public int strength;
+
+    public Cat() {
+    }
+
+    public boolean fight(Cat anotherCat) {
+        //write your code here
+    }
+    public static void main(String[] args) {
+
+    }
+}
+    
