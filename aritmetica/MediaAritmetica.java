9f4fe9f38822a993bce83c4171de0d6c7398ce03
Bruna <bruna.dev@hotmail.com>
2024-03-24 19:12:03 -0300

    feat: adicionando logica que exiba a media aritmetica

--- aritmetica/MediaAritmetica.java
+++ aritmetica/MediaAritmetica.java
@@ -1,6 +1,23 @@
 package aritmetica;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class MediaAritmetica {
     public static void main(String[] args) throws Exception {
-        //write your code here
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+        double sum = 0;
+        int count = 0;
+        while (true) {
+            int a = Integer.parseInt(bis.readLine());
+
+            if (a == -1) {
+                System.out.println(sum / count);
+                return;
+            }
+            count++;
+            sum += a;
+        }
     }
 }
