c9480fea1dca72ca1e51dc291477ab7134fa5914
Bruna <bruna.dev@hotmail.com>
2024-04-01 21:28:33 -0300

    feat: adicionando String para nome, int para idasde e char para sexo

--- friendclass/Friend.java
+++ friendclass/Friend.java
@@ -1,8 +1,27 @@
 package friendclass;
 
 public class Friend {
-    //write your code here
-    public static void main(String[] args) {
-    
+    public String nome;
+    public int age;
+    public char sex;
+
+    public void initialize(String nome) {
+        this.nome = nome;
     }
+
+    public void initialize(String nome, int age) {
+        this.nome = nome;
+        this.age = age;
+    }
+
+    public void initialize(String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
