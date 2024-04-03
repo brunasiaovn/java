c45fe36ed21d1120e0da12edb2856fc110a6b988
Bruna <bruna.dev@hotmail.com>
2024-04-02 22:29:42 -0300

    feat: adicionando String e Int, com método que inicialize o  nome, idade, peso e cor

--- inicializandogato/Gato.java
+++ inicializandogato/Gato.java
@@ -1,7 +1,45 @@
 package inicializandogato;
 
 public class Gato {
-    //write your code here
+    public String nome;
+    public int idade;
+    public int peso;
+    public String endereço;
+    public String cor;
+
+    public void initialize(String nome) {
+        this.nome = nome;
+        this.idade = 1;
+        this.peso = 1;
+        this.cor = "Preto";
+    }
+
+    public void initialize(String nome, int peso, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.cor = "Preto";
+    }
+
+    public void initialize(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = 1;
+        this.cor = "Preto";
+    }
+
+    public void initialize(int peso, String cor) {
+        this.peso = peso;
+        this.cor = cor;
+        this.idade = 1;
+    }
+
+    public void initialize(int peso, String cor, String endereço) {
+        this.peso = peso;
+        this.endereço = endereço;
+        this.cor = cor;
+        this.idade = 1;
+    }
 
     public static void main(String[] args) {
 
