ead5e9ec60a4676456544c8d5a990afb211b2694
Bruna <bruna.dev@hotmail.com>
2024-04-08 22:26:26 -0300

    feat: adicionando int e método que inicializa left, top, width, height

--- rectangle/Retangulo.java
+++ rectangle/Retangulo.java
@@ -1,7 +1,40 @@
 package rectangle;
 
 public class Retangulo {
-    //escreva seu código aqui
+    public int left;
+    public int top;
+    public int width;
+    public int height;
+
+    public void initialize(int left, int top, int width, int height) {
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void initialize(int left, int top) {
+        this.left = left;
+        this.top = top;
+        this.width = 0;
+        this.height = 0;
+    }
+
+    public void initialize(int left, int top, int width) {
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = width;
+    }
+
+    public void initialize(Retangulo retangulo) {
+        this.left = retangulo.left;
+        this.top = retangulo.top;
+        this.width = retangulo.width;
+        this.height = retangulo.height;
+    }
+
     public static void main(String[] args) {
+
     }
 }
