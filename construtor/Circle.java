725182f3af860e980e75b4a4478e48d90d72d6ec
Bruna <bruna.dev@hotmail.com>
2024-04-15 23:21:45 -0300

    feat(adiciona lógica que exibi a palavra red na tela)

--- construtor/Circle.java
+++ construtor/Circle.java
@@ -1,16 +1,15 @@
 package construtor;
 public class Circle {
-    
     public Color color;
+
+    public Circle() {
+        color = new Color();
+    }
 
     public static void main(String[] args) {
         Circle circle = new Circle();
         circle.color.setDescription("Red");
         System.out.println(circle.color.getDescription());
-    }
-
-    public void Circle() {
-        color = new Color();
     }
 
     public class Color {
