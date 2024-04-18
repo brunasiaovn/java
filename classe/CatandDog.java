a929fe2ce6e88fbd4ccdb8ca87eb5777e80debb8
Bruna <bruna.dev@hotmail.com>
2024-04-17 21:54:35 -0300

    feat(adiciona objeto Cat e objeto Dog)

--- classe/CatandDog.java
+++ classe/CatandDog.java
@@ -1,19 +1,21 @@
 package classe;
 
-/* 
-The whole duck isn't enough
-
-*/
-
 public class CatandDog {
-
     public static void main(String[] args) {
         Duck duck1 = new Duck();
         Duck duck2 = new Duck();
         System.out.println(duck1);
         System.out.println(duck2);
 
-        //write your code here
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        System.out.println(cat1);
+        System.out.println(cat2);
+
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+        System.out.println(dog1);
+        System.out.println(dog2);
     }
 
     public static class Duck {
@@ -22,5 +24,15 @@ public class CatandDog {
         }
     }
 
-    //write your code here
+    public static class Cat {
+        public String toString() {
+            return "Cat";
+        }
+    }
+
+    public static class Dog {
+        public String toString() {
+            return "Dog";
+        }
+    }
 }
