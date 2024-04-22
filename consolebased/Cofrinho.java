a33208237d003701a480cb5f54b8e91c8c088da1
Bruna <bruna.dev@hotmail.com>
2024-04-22 12:47:59 -0300

    feat(Adiciona import BufferedReader e InputStreamReader, adiciona classe Cofrinho e método main inicial)

---
+++ consolebased/Cofrinho.java
@@ -0,0 +1,10 @@
+package consolebased;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+ public class Cofrinho {
+    public static void main(String[] args) throws Exception {
+        //Escreva seu código aqui
+    }
+}
