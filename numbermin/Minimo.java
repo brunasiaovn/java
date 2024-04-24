eeb665cd48c52d79461d13d98f4238ad17da5162
Bruna <bruna.dev@hotmail.com>
2024-04-23 23:58:09 -0300

    feat(Adiciona lógica que exibe string que termina com o mínimo dos 5 números inseridos)

--- numbermin/Minimo.java
+++ numbermin/Minimo.java
@@ -9,14 +9,8 @@ public class Minimo {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(reader.readLine());
         int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());
 
-        int minimum = min(a, b);
-
-        System.out.println("Minimum = " + minimum);
-    }
-
-
-    public static int min(int a, int b) {
-        return a < b ? a : b;
-    }
-}
+        int min
