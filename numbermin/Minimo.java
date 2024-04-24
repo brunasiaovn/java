af69df74279ac0d8d350bda8a552ca45073cc8f0
Bruna <bruna.dev@hotmail.com>
2024-04-23 23:56:02 -0300

    feat(Adiciona import BufferedReader e InputStreamReader, adiciona classe Minimo, método main inicial e int)

---
+++ numbermin/Minimo.java
@@ -0,0 +1,22 @@
+package numbermin;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Minimo {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+
+        int minimum = min(a, b);
+
+        System.out.println("Minimum = " + minimum);
+    }
+
+
+    public static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+}
