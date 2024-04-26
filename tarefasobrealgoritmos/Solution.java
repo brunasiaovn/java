319aac17aefc8bd95ffda96b28e72663650e8500
Bruna <bruna.dev@hotmail.com>
2024-04-25 21:46:42 -0300

    feat(Adiciona classe Solution, BufferedReader e método main inicial)

---
+++ tarefasobrealgoritmos/Solution.java
@@ -0,0 +1,14 @@
+package tarefasobrealgoritmos;
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int maximum = ;
+
+        //Digite seu código aqui
+
+        System.out.println(maximum);
+    }
+}
