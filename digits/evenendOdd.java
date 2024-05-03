861d1c8a86777bb31ad0210fa3819070b7674812
Bruna <bruna.dev@hotmail.com>
2024-05-02 21:41:34 -0300

    feat(Adiciona classe evenendOdd, int e metódo main inicial)

---
+++ digits/evenendOdd.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class evenendOdd {
+
+    public static int even;
+    public static int odd;
+
+    public static void main(String[] args) throws IOException {
+        //Digite seu código aqui
+    }
+}
