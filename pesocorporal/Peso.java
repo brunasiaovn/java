9e72cfd8c2052bce73bdef64bd56d59082a76739
Bruna <bruna.dev@hotmail.com>
2024-05-06 20:31:20 -0300

    fet(Adiciona método calculateBMI que exiba o índice de massa corporal

--- pesocorporal/Peso.java
+++ pesocorporal/Peso.java
@@ -1,5 +1,7 @@
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Peso {
  
@@ -13,7 +15,16 @@ public class Peso {
 
     public static class Body {
         public static void calculateBMI(double weight, double height) {
-            // Escreva seu código aqui
+            double index = weight / (height * height);
+            if (index < 18.5) {
+                System.out.println("Underweight: BMI < 18.5");
+            } else if (index >= 18.5 && index < 25) {
+                System.out.println("Normal: 18.5 <= BMI < 25");
+            } else if (index >= 24.9 && index < 30) {
+                System.out.println("Overweight: 25 <= BMI < 30");
+            } else if (index >= 30) {
+                System.out.println("Obese: BMI >= 30");
+            }
         }
     }
 }
