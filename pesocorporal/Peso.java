6270ee06cd64399e2e7817a000e6017327f43919
Bruna <bruna.dev@hotmail.com>
2024-05-05 22:38:32 -0300

    docs(Adiciona classe Peso e método main e método calculate usando double)

---
+++ pesocorporal/Peso.java
@@ -0,0 +1,19 @@
+
+import java.io.*;
+
+public class Peso {
+ 
+    public static void main(String[] args) throws IOException {
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+        double weight = Double.parseDouble(bis.readLine());
+        double height = Double.parseDouble(bis.readLine());
+
+        Body.calculateBMI(weight, height);
+    }
+
+    public static class Body {
+        public static void calculateBMI(double weight, double height) {
+            // Escreva seu código aqui
+        }
+    }
+}
