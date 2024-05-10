46b4a074ae5a788f42c904d6da048c8ed98f031e
Bruna <bruna.dev@hotmail.com>
2024-05-09 22:58:33 -0300

    feat(Adiciona import, classe Matriz, método main que inicializa um array e max)

---
+++ matriz/Matriz.java
@@ -0,0 +1,23 @@
+package matriz;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Matriz {
+    public static void main(String[] args) throws Exception {
+        int[] array = initializeArray();
+        int max = max(array);
+        System.out.println(max);
+    }
+
+    public static int[] initializeArray() throws IOException {
+        // Crie e preencha o array
+        return null;
+    }
+
+    public static int max(int[] array) {
+        // Encontre o máximo
+        return 0;
+    }
+}
