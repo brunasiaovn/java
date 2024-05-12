8e5d906cceee3890d3ebe4fd3df6ddf37a312a83
Bruna <bruna.dev@hotmail.com>
2024-05-11 23:19:20 -0300

    feat(Adiciona import BufferedReader, import InputStreamReader, classe Inversa e método main inicial)

---
+++ matrizinversa/Inversa.java
@@ -0,0 +1,10 @@
+package matrizinversa;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Inversa {
+    public static void main(String[] args) throws Exception {
+        
+        //Escreva seu código aqui
+    }
+}
