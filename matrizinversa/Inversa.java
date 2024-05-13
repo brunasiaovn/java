de557b60d28877c4ed2ac32633f9ed10098b369f
Bruna <bruna.dev@hotmail.com>
2024-05-12 23:40:51 -0300

    feat(Adiciona lógica que o Array de strings deve ser exibido em ordem reversa)

--- matrizinversa/Inversa.java
+++ matrizinversa/Inversa.java
@@ -2,9 +2,17 @@ package matrizinversa;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-class Inversa {
+public class Inversa {
     public static void main(String[] args) throws Exception {
-        
-        //Escreva seu código aqui
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] array = new String[10];
+        for (int i = 0; i < 8; i++) {
+            array[i] = bis.readLine();
+        }
+
+        for (int i = 9; i >= 0; i--) {
+            System.out.println(array[i]);
+        }
     }
 }
