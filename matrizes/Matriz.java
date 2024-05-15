d2a33c6201f42aef46eb3ab5e1b14a1e16954dd6
Bruna <bruna.dev@hotmail.com>
2024-05-14 21:16:59 -0300

    feat(Adiciona classe Matriz e método main inicial)

---
+++ matrizes/Matriz.java
@@ -0,0 +1,10 @@
+package matrizes;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Matriz {
+    public static void main(String[] args) throws Exception {
+        // Escreva seu código aqui
+    }
+}
