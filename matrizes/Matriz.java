5f1c115ea93ae8554436c7068033d9630934a0ec
Bruna <bruna.dev@hotmail.com>
2024-05-15 23:40:59 -0300

    feat(Divisão dos números digitados em dois conjuntos)

--- matrizes/Matriz.java
+++ matrizes/Matriz.java
@@ -5,6 +5,23 @@ import java.io.InputStreamReader;
 
 public class Matriz {
     public static void main(String[] args) throws Exception {
-        // Escreva seu código aqui
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] array = new int[20];
+        for (int i = 0; i < 20; i++) {
+            array[i] = Integer.parseInt(bis.readLine());
+        }
+
+        int[] array1 = new int[10];
+        int[] array2 = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array1[i] = array[i];
+            array2[i] = array[i + 10];
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(array2[i]);
+        }
+
     }
 }
