f6c47af650d73cd43256604accb037d6bc210d98
Bruna <bruna.dev@hotmail.com>
2024-05-17 23:59:41 -0300

    fix(Corrigido erro de inicialização da variável 'maximu)m'

--- tarefasobrealgoritmos/Solution.java
+++ tarefasobrealgoritmos/Solution.java
@@ -5,10 +5,17 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int maximum = ;
+        int maximum = Integer.MIN_VALUE; // Inicialize com o menor valor possível
 
-        //Digite seu código aqui
+        String input;
+        while ((input = reader.readLine()) != null && !input.isEmpty()) {
+            int number = Integer.parseInt(input);
+            if (number > maximum) {
+                maximum = number;
+            }
+        }
 
-        System.out.println(maximum);
+        System.out.println("O máximo é: " + maximum);
     }
 }
+
