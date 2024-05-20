94d2695965363b9ddcad466cbd005beb4df5858f
Bruna <bruna.dev@hotmail.com>
2024-05-20 20:16:32 -0300

    fix(Corrigir lógica de cálculo de preço das maçãs)

--- calculeocusto/CustototaldasMacas.java
+++ calculeocusto/CustototaldasMacas.java
@@ -2,10 +2,8 @@ package calculeocusto;
 
 public class CustototaldasMacas {
     public static void main(String[] args) {
-        Apple apple = new Apple();
-        apple.addPrice(50);
-        Apple apple2 = new Apple();
-        apple2.addPrice(100);
+        Apple.addPrice(50);
+        Apple.addPrice(100);
         System.out.println("The cost of apples is" + Apple.applePrice);
     }
 
