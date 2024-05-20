fe5e24ffdd8f8f7f591ab0d47027091a68621d05
Bruna <bruna.dev@hotmail.com>
2024-05-19 22:38:43 -0300

    Fix(Correção de erro na leitura de array e cálculo do máximo)

--- matriz/Matriz.java
+++ matriz/Matriz.java
@@ -4,17 +4,31 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Matriz {    public static void main(String[] args) throws Exception {
-    int[] array = initializeArray();
+public class Matriz {
+    public static void main(String[] args) throws Exception {
+        int[] array = new int[20]; 
+        initializeArrayValues(array); 
 
-    int max = max(array);
+        int max = max(array);
 
-    System.out.println(max);
+        System.out.println(max);
+    }
+
+    public static void initializeArrayValues(int[] array) throws IOException {
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 20; i++) {
+            array[i] = Integer.parseInt(bis.readLine());
+        }
+    }
+
+    public static int max(int[] array) {
+        int max = array[0]; 
+        for (int i = 1; i < 20; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
 }
-
-public static int[] initializeArray() throws IOException {
-    BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
-
-    int[] array = new int[20];
-    for (int i = 0; i < 20; i++) {
-        array[i] = Integer.parse
