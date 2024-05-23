b85b95de3e1c9da45908a688656c7b4d10e95fa8
Bruna <bruna.dev@hotmail.com>
2024-05-22 21:23:28 -0300

    fix(removendo string name)

--- maiordedezoito/MaiorIdade.java
+++ maiordedezoito/MaiorIdade.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 public class MaiorIdade {
     public static void main(String[] args) throws Exception {
         BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
-        String name = bis.readLine();
         int idade = Integer.parseInt(bis.readLine());
 
         if (idade < 18) {
