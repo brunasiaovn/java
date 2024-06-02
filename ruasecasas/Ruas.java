82f4094ac68a6061a2f70100d86ad55da3464d75
Bruna <bruna.dev@hotmail.com>
2024-06-02 20:27:08 -0300

    feat(Adiciona classe Ruas, método main e import BufferedReader, IOexception, InputStreamReader)

---
+++ ruasecasas/Ruas.java
@@ -0,0 +1,10 @@
+package ruasecasas;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ruas {
+    public static void main(String[] args) throws Exception {
+        //Digite seu código aqui
+    }
+}
