dd2abd45df364eb8e44b90cf5aaeb6687a8f6899
Bruna <bruna.dev@hotmail.com>
2024-06-04 23:39:54 -0300

    feat(Implementa lógica para comparar residentes em casas de números pares e ímpares)

--- ruasecasas/Ruas.java
+++ ruasecasas/Ruas.java
@@ -4,7 +4,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Ruas {
-    public static void main(String[] args) throws Exception {
-        //Digite seu código aqui
+    public static void main(String[] args) throws IOException {
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] array = new int[15];
+        for (int i = 0; i < 15; i++) {
+            array[i] = Integer.parseInt(bis.readLine());
+        }
+
+        int sum1 = 0;
+        int sum2 = 0;
+
+        for (int i = 0; i < 15; i++) {
+            if (i % 2 == 0) {
+                sum1 += array[i];
+            } else {
+                sum2 += array[i];
+            }
+        }
+
+        if (sum1 > sum2) {
+            System.out.println("Even-numbered houses have more residents.");
+        } else if (sum2 > sum1) {
+            System.out.println("Odd-numbered houses have more residents.");
+        }
     }
 }
