0bc2874735d613f99dadc2c7f7ce252fbcc157e2
Bruna <bruna.dev@hotmail.com>
2024-06-16 22:03:27 -0300

    feat(adiciona classe ListaReversa, import BufferedReader, import InputStreamReader, import ArrayList, import Collections e método main inicial)

---
+++ listareversa/ListaReversa.java
@@ -0,0 +1,12 @@
+package listareversa;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ListaReversa {
+    public static void main(String[] args) throws Exception {
+        //Escreva seu código aqui
+    }
+}
+
