310097efa9621dc6326a249706bd980ad50adb55
Bruna <bruna.dev@hotmail.com>
2024-06-16 23:09:15 -0300

    feat(adiciona funcionalidade para ler 5 strings, remover a 3ª, e exibir o restante em ordem reversa)

--- listareversa/ListaReversa.java
+++ listareversa/ListaReversa.java
@@ -1,4 +1,5 @@
 package listareversa;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -6,7 +7,19 @@ import java.util.Collections;
 
 public class ListaReversa {
     public static void main(String[] args) throws Exception {
-        //Escreva seu código aqui
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> lista = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            String s = bis.readLine();
+            lista.add(s);
+        }
+
+        lista.remove(2);
+        Collections.reverse(lista);
+
+        for (String s : lista) {
+            System.out.println(s);
+        }
     }
 }
-
