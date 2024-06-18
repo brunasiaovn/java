593d1986acb5efe8d3c93a3a292da6a28f59fed3
Bruna <bruna.dev@hotmail.com>
2024-06-17 23:26:33 -0300

    feat(adiciona import BufferedReader, import InputStreamReader, import ArrayList, classe SamIAm e método main inicial)

---
+++ samiamlist/SamIAm.java
@@ -0,0 +1,11 @@
+package samiamlist;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class SamIAm {
+    public static void main(String[] args) throws Exception {
+        //escreva seu código aqui
+    }
+}
