110f694813f0d418b58042d40cc1d6958623d243
Bruna <bruna.dev@hotmail.com>
2024-06-20 21:57:51 -0300

    feat(Adiciona a classe DuplicandoPalavras, método main, método doubleValues e prepara a leitura de strings do console e a exibição do resultado)

---
+++ palavras/DuplicandoPalavras.java
@@ -0,0 +1,20 @@
+package palavras;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class DuplicandoPalavras {
+    public static void main(String[] args) throws Exception {
+        // Leia strings do console e declare um ArrayList aqui
+
+        ArrayList<String> resultado = doubleValues(lista);
+
+        // Exibir resultado
+    }
+
+    public static ArrayList<String> doubleValues(ArrayList<String> lista) {
+        // Escreva seu código aqui
+        return null;
+    }
+}
